7b06ce4533d9cd487adc5ea8efcb36f054637ad8
Edward <edward@louths.org.uk>
2020-04-01 11:34:36 +0900

    updates to Utils.AST

--- src/Utils/AST.php
+++ src/Utils/AST.php
@@ -403,13 +403,15 @@ class AST
             }
 
             $coercedObj = [];
-            $fields     = $type->getFields();
             $fieldNodes = Utils::keyMap(
                 $valueNode->fields,
                 static function ($field) {
                     return $field->name->value;
                 }
             );
+
+	    $fields = array_merge(array_flip(array_map(function($item) { return $item->name->value; }, $fieldNodes)), $type->getFields());
+
             foreach ($fields as $field) {
                 /** @var ValueNode $fieldNode */
                 $fieldName = $field->name;
